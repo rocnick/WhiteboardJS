25110e33d38be5459264ec86913f9410f0b998d6
Nick Snyder <me@nicksnyder.info>
2015-03-05 02:54:31 -0500

    UI dev

--- public/javascripts/whiteboard.js
+++ public/javascripts/whiteboard.js
@@ -1,6 +1,21 @@
 //  Project:   Whiteboard JS
 //  Author:    Nick Snyder
-(function whiteboard()
-{
-  //
-})();
+var whiteboard = function() {
+  document.getElementsByTagName('body')[0].onload = this.resizeWorkspace;
+  window.onresize = this.resizeWorkspace;
+};
+
+whiteboard.prototype = {
+  resizeWorkspace: function()
+  {
+    var body = document.getElementsByTagName('body')[0];
+    var header = document.getElementsByTagName('header')[0];
+    var board = document.getElementById('board');
+    var palatte = document.getElementById('palatte');
+
+    palatte.style.height = board.style.height = parseInt(body.offsetHeight) - parseInt(header.offsetHeight) + 'px';
+    palatte.style.marginTop = board.style.marginTop = parseInt(header.offsetHeight) + 'px';
+  }
+};
+
+new whiteboard();
