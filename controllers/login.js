b46d0788fdbf589b7be6b5e0fb05e0888a908382
= <=>
2015-03-08 10:45:09 -0400

    Updating post route for login

---
+++ controllers/login.js
@@ -0,0 +1,5 @@
+module.exports = {
+  verifyCredentials: function() {
+    return true;
+  }
+};
--- routes/login.js
+++ routes/login.js
@@ -2,11 +2,20 @@
 //  Author:     Nick Snyder
 
 var express = require('express');
+var login = require('../controllers/login');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('login', { title: 'WhiteboardJS' });
+});
+
+router.post('/', function(req, res) {
+  var credentials = req.body;
+  if(login.verifyCredentials())
+    res.send('successful login');
+  else
+    res.send('failed login');
 });
 
 module.exports = router; 
